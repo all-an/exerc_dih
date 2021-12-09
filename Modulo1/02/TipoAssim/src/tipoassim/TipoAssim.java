8ae12c23fd957520aef68c4657e08d81e7f55cfc
all-an <all-an@hotmail.com>
2021-12-09 08:14:25 -0300

    learning

---
+++ Modulo1/02/TipoAssim/src/tipoassim/TipoAssim.java
@@ -0,0 +1,14 @@
+package tipoassim;
+
+import java.util.Scanner;
+
+public class TipoAssim {
+	public static void main(String[] args) {
+		System.out.println("1 Números Inteiros (tipo int)\n2 Números Reais (tipo double)\n"
+				+ "3 Caracteres (tipo char)\n4 Booleano (tipo boolean)");
+		System.out.println("Exemplo, digite 1 para ver sobre inteiros."
+				+ "\nEscolha o tipo para ver a descrição:");
+		Scanner sc = new Scanner(System.in);
+	}
+
+}
