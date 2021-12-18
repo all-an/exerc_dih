9acf14deb0e3d4fd9d66373f4bf91e8ef688a536
all-an <all-an@hotmail.com>
2021-12-17 21:16:06 -0300

    exercicio

--- Modulo1/Semana1/05/TamanhoNome/src/tamanhonome/TamanhoNome.java
+++ Modulo1/Semana1/05/TamanhoNome/src/tamanhonome/TamanhoNome.java
@@ -5,22 +5,21 @@ import java.util.Scanner;
 public class TamanhoNome {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Entre uma palavra: ");
+		System.out.println("Entre um nome: ");
 
 		int caracteresLetras = 0, outrosCaracteres = 0;
 
 		String entrada = scanner.nextLine();
 		for (int i = 0; i < entrada.length(); i++) {
-			
 			if (!Character.isLetter(entrada.charAt(i))) {
 				outrosCaracteres++;
 			} else {
 				caracteresLetras++;
 			}
 		}
-		System.out.println("O tamanho da palavra é: " + caracteresLetras + " letras.");
+		System.out.println("O tamanho do nome é: " + caracteresLetras + " letras.");
 		if (outrosCaracteres > 0) {
-			System.out.println("Você digitou também " + outrosCaracteres + " outros caracteres.");
+			System.out.println("Você digitou também " + outrosCaracteres + " outros caracteres e espaços.");
 		}
 	}
 }
